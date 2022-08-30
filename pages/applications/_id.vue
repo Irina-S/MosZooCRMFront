52a9d97aa8e65c4ef3324f25ec2fd66b0a8c5a07
imser <imser@hismith.ru>
2022-08-30 22:20:32 +0800

    fix T16283 Группа в статусе Запрос документов для пони-клуб

--- pages/applications/_id.vue
+++ pages/applications/_id.vue
@@ -454,6 +454,7 @@ export default {
         this.application.type === 'pony_club' &&
         (this.isAdmin ||
           [
+            Status.DOCUMENTS_REQUEST,
             Status.INVITATION_TO_ENTRANCE_EXAMINATIONS,
             Status.APPROVED_BY_EXAMINATIONS,
             Status.COMPLETED,
@@ -573,7 +574,7 @@ export default {
               this.examinations.date,
               this.examinations.time
             )
-            // params.group_id = this.application.child_group_id
+            params.group_id = this.application.child_group_id
             if (this.application.receipt_documents_at) {
               params.receipt_documents_at =
                 this.application.receipt_documents_at
